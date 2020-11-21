53867fb2366fcac847c14ef7dedc068a4eb440b8
Noe Rios <noerios@gmail.com>
2020-11-20 21:09:22 -0600

    initial html and js work

---
+++ Assets/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    const myApiKey = 'd5ab1b02d29d3efac51697cf7938b1c6'
+
+    $('#searchBtn').click(function () {
+        let userInput = $('#city').val();
+        let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${myApiKey}`;
+//Call current weather ajax
+        $.ajax({
+            url: queryURL,
+            type: 'GET',
+
+        }).then(function (response) {
+
+            console.log(response);
+            for (let i = 0; i < response.list.length; i++) {
+                let hour = response.list[i];
+                if (hour.dt_txt.indexOf('00:00:00') != -1) {
+                    let date = new Date(hour.dt_txt).toLocaleDateString();
+                     hour.main.temp;
+                    hour.main.humidity;
+                    let DIV = $('<span>');
+                    let image = $('<img>');
+                    image.attr(
+                      'src',
+                        `http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`
+                    );
+                    DIV.addClass('divClasses');
+                    DIV.append(`<h3>${date}</h3>`);
+                    DIV.append(image);
+                    DIV.append(
+                     `<p>Temperture <br>  ${
+                        hour.main.temp + 'º F'
+                      } </p><p>Humidity <br> ${hour.main.humidity + '%'}</p>`
+                    );
+                    $('#fiveDayForecast').append(DIV);
+                }
+            }
+        });
+    })    
+})
