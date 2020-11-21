a64d5000ca791aeb94d93e0f669434d29c159bf1
Noe Rios <noerios@gmail.com>
2020-11-20 21:34:32 -0600

    improvements to api, now working

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     const myApiKey = '2c6bf35b79f11e55f6ca64bbb24dbb3e'
 
-    $('#searchBtn').click(function () {
+$('#searchBtn').click(function () {
         let userInput = $('#city').val();
         let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${myApiKey}`;
 //Call current weather ajax
@@ -10,31 +10,27 @@ $(document).ready(function () {
             type: 'GET',
 
         }).then(function (response) {
-
             console.log(response);
-            for (let i = 0; i < response.list.length; i++) {
-                let hour = response.list[i];
-                if (hour.dt_txt.indexOf('00:00:00') != -1) {
-                    let date = new Date(hour.dt_txt).toLocaleDateString();
-                     hour.main.temp;
-                    hour.main.humidity;
-                    let DIV = $('<span>');
-                    let image = $('<img>');
-                    image.attr(
-                      'src',
-                        `http://openweathermap.org/img/wn/${hour.weather[0].icon}.png`
-                    );
-                    DIV.addClass('divClasses');
-                    DIV.append(`<h3>${date}</h3>`);
-                    DIV.append(image);
-                    DIV.append(
-                     `<p>Temperture <br>  ${
-                        hour.main.temp + 'º F'
-                      } </p><p>Humidity <br> ${hour.main.humidity + '%'}</p>`
-                    );
-                    $('#fiveDayForecast').append(DIV);
-                }
-            }
+            response.name;
+            response.main.temp;
+            response.main.humidity;
+            response.wind.speed;
+            $('#cityName').text(
+              response.name + ' (' + new Date().toLocaleDateString() + ')'
+            );
+            $('#cityName').append(
+              `<img src="http://openweathermap.org/img/wn/${response.weather[0].icon}.png">`
+            );
+            $('#currentTemp')
+              .text(`Temperature: ${response.main.temp}` + 'º F')
+              .addClass('currentWeather');
+            $('#humidity')
+              .text(`Humidity: ${response.main.humidity}` + '%')
+              .addClass('currentWeather');
+            $('#windSpeed')
+              .text(`Windspeed: ${response.wind.speed}` + 'mph')
+              .addClass('currentWeather');
+            getForcast(userInput);
         });
-    })    
-})
+    });    
+});
